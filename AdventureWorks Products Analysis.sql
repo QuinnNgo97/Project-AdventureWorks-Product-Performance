51f055ad11856f4aa0e4d5948b10530b33932f65
Quynh Anh Ngo <76810409+QuinnNgo97@users.noreply.github.com>
2025-01-02 06:20:43 +0700

    Update AdventureWorks Products Analysis.sql

--- AdventureWorks Products Analysis.sql
+++ AdventureWorks Products Analysis.sql
@@ -1,9 +1,9 @@
 -- Create temp table to house SalesOrderDetail:
-WITH cte AS (SELECT SalesOrderID, OrderDate, TerritoryID FROM sales.SalesOrderHeader
+WITH cte AS (SELECT SalesOrderID, OrderDate, TerritoryID FROM [AdventureWorks2022].sales.SalesOrderHeader
 WHERE OrderDate BETWEEN '2012-05-30' AND '2014-05-30')
 
 SELECT cte.*, s.ProductID, s.OrderQty, s.SpecialOfferID, s.UnitPrice, s.LineTotal INTO #TempSales
-FROM Sales.SalesOrderDetail s
+FROM [AdventureWorks2022].Sales.SalesOrderDetail s
 
 -- Inner join SalesOrderHeader
 INNER JOIN 
@@ -14,23 +14,23 @@ SELECT * FROM #TempSales
 -- Create temp table to house SalesTerritory:
 SELECT TerritoryID, [Name], CountryRegionCode, [Group]
 INTO #TempTerritory
-FROM Sales.SalesTerritory;
+FROM [AdventureWorks2022].Sales.SalesTerritory;
 
 -- Create temp table to house ProductInventory:
 SELECT ProductID, Quantity
 INTO #TempInventory
-FROM Production.ProductInventory;
+FROM [AdventureWorks2022].Production.ProductInventory;
 
 -- Create cte to house Product that are Salable Products: FinishedGoodsFlag = 1
 WITH cte2 AS (SELECT ProductID, [Name], StandardCost, ListPrice, ProductSubcategoryID, ProductLine, Class, Style, [Size], SafetyStockLevel, Color, MakeFlag
-FROM Production.Product tp
+FROM [AdventureWorks2022].Production.Product tp
 WHERE FinishedGoodsFlag = 1)
 
 -- Add productcategoryname + productsubcategoryname and create a temp table to house Product info:
 SELECT cte2.* , ps.Name AS SubCatName, ps.ProductCategoryID, pc.Name AS CatName INTO #TempProduct FROM cte2 
-LEFT JOIN Production.ProductSubcategory AS ps
+LEFT JOIN [AdventureWorks2022].Production.ProductSubcategory AS ps
 ON cte2.ProductSubcategoryID = ps.ProductSubcategoryID
-LEFT JOIN Production.ProductCategory AS pc
+LEFT JOIN [AdventureWorks2022].Production.ProductCategory AS pc
 ON ps.ProductCategoryID = pc.ProductCategoryID;
 
 -- Drop CategoryID column:
